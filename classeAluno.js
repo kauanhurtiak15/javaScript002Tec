30d4f6c7237264734e4cb06293fe16b6a88b7aa4
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-04-15 12:44:08 +0000

    OK

---
+++ classeAluno.js
@@ -0,0 +1,51 @@
+class Aluno{
+    constructor(nome, matricula, notas){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.notas = notas;
+
+    }
+
+    calcularMedia(){
+        let soma = 0;
+        for (let i = 0; i < this.notas.length; i++){
+            soma += this.notas
+        }
+        return soma / this.notas.length
+    }
+
+    obterStatus(){
+        const media = this.calcularMedia()
+
+        if (media >= 7){
+            return "Aprovado"
+        } else if (media >= 5){
+            return "Recuperação"
+        } else{
+            return "reprovado"
+        }
+}
+        exibirRelatorio(){
+            console.log(`Relatorio do aluno ${this.nome}`);
+            console.log(`Matricula ${this.matricula}`);
+            console.log(`Media Final ${this.calcularMedia()}`)
+            console.log(`Status ${this.obterStatus()}`);
+}
+
+
+}
+
+const turma = [
+    new Aluno(`Luiz`, `CGM5050`, [7.2, 6.5, 7.2]),
+    new Aluno(`Ana`, `CGM5051`, [3.5, 4.3, 9.2]),
+    new Aluno(`Tamirys`, `CGM5052`, [6.5, 5.3, 4.2]),
+    new Aluno(`Arthur`, `CGM5053`, [6.5, 7.2, 9.2])
+]
+
+
+console.log(turma[1]);
+
+// let aluno1 = new Aluno(`Luiz`, 850850, [7.2, 6.5, 3.3]);
+// console.log(aluno1.calcularMedia());
+// console.log(aluno1.obterStatus());
+// aluno1.exibirRelatorio()
